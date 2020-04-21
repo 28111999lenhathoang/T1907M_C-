2bf15165e53531872252c6594ba931662adb3a85
28111999lenhathoang <53230244+28111999lenhathoang@users.noreply.github.com>
2020-04-22 01:03:26 +0700

    assignment3

---
+++ Assignment3/Cart.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using T1907M.Properties.session2;
+
+namespace T1907M.Assignment3
+{
+    public class Cart : Product
+    {
+        public string id { set; get; }
+        public string customer { set; get; }
+        public int grandTotal { set; get; }
+        public string city { set; get; }
+        public string country { set; get; }
+        List<Product> productList = new List<Product>();
+
+        public void ThemSanPham()
+        {
+            productList.Add(new Product());
+        }
+
+        public void XoaSanPham()
+        {
+            productList.RemoveAt(Convert.ToInt32(Console.ReadLine()));
+        }
+
+        public void TongTien()
+        {
+            if (country == "Viet Name")
+            {
+                if (city == "ha noi" || city == "ho chi minh")
+                {
+                    grandTotal = Convert.ToInt32(price*1.01);
+                }
+                else
+                {
+                    grandTotal = Convert.ToInt32(price * 1.02);
+                }
+            }
+            else
+            {
+                grandTotal = Convert.ToInt32(price * 1.05);
+            }
+        }
+        
+    }
+}
---
+++ Assignment3/programs.cs
@@ -0,0 +1,14 @@
+﻿namespace T1907M.Assignment3
+{
+    public class programs
+    {
+        public static void Main(string[] args)
+        {
+            Cart x = new Cart();
+            x.ThemSanPham();
+            x.XoaSanPham();
+            x.TongTien();
+        }
+        
+    }
+}
