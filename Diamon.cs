b4724b421888376a459956b0ebdc1c027cc2e7b1
28111999lenhathoang <53230244+28111999lenhathoang@users.noreply.github.com>
2020-04-22 01:04:09 +0700

    lab3

---
+++ Diamon.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace T1907M.Properties.session2
+{
+    public class Diamon
+    {
+        public int cara;
+        public int trongluong;
+
+        public void KiemTra_real_fake()
+        {
+            if (cara < 1)
+            {
+                Console.WriteLine("hang gia");
+            }
+            else
+            {
+                Console.WriteLine("hang that");
+            }
+        }
+    }
+}
---
+++ Fashion.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Runtime.Remoting.Metadata.W3cXsd2001;
+
+namespace T1907M.Properties.session2
+{
+    public class Fashion : Product
+    {
+        public string color;
+        public string size;
+
+        public void Kiemtra()
+        {
+            Console.WriteLine("Nhap id");
+            string a = Convert.ToString(Console.ReadLine());
+            foreach (Product x in gallery)
+            {
+                if (x.id == a)
+                {
+                    Console.WriteLine("con hang");
+                }
+                else
+                {
+                    Console.WriteLine("het hang");
+                }
+            }
+        }
+    }
+}
