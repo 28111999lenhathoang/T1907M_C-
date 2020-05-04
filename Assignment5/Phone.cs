ceea5ec07adbda8c2bf3297b5709664efdcc5587
28111999lenhathoang <53230244+28111999lenhathoang@users.noreply.github.com>
2020-05-04 10:36:08 +0700

    Assiment5

---
+++ Assignment5/Phone.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace T1907M.Assignment5
+{
+    public abstract class Phone
+    {
+        protected string name;
+        protected string phone;
+
+        public Phone()
+        {
+            
+        }
+        public Phone(string phone)
+        {
+            this.phone = phone;
+        }
+        public Phone(string name, string phone)
+        {
+            this.name = name;
+            this.phone = phone;
+        }
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public string Phone1
+        {
+            get { return phone; }
+            set { phone = value; }
+        }
+
+        protected abstract void InsertPhone(string name, string phone);
+        protected abstract void RemovePhone(string name);
+        protected abstract void UpdatePhone(string name, string newPhone);
+        protected abstract void SearchPhone(string name);
+
+        public void Showlist()
+        {
+            Console.WriteLine("name : " +name);
+            Console.WriteLine("phone : " +phone);
+        }
+    }
+}
---
+++ Assignment5/PhoneBook.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace T1907M.Assignment5
+{
+    public class PhoneBook : Phone
+    {
+        public static void Main(string[] args)
+        {
+            List<PhoneBook> PhoneList = new List<PhoneBook>();
+            PhoneBook a = new PhoneBook();
+            PhoneList.Add(new PhoneBook("le nhat hoang", "0394134127"));
+            Console.WriteLine("danh sach");
+            foreach (PhoneBook x in PhoneList)
+            {
+                x.Showlist();
+            }
+
+            a.InsertPhone("le thi quynh trang", "0968923122");
+            a.RemovePhone("le nhat hoang");
+            a.SearchPhone("le thi quynh trang");
+            a.UpdatePhone("le thi quynh trang", "1999");
+            Console.WriteLine("danh sach sau update");
+            foreach (PhoneBook x in PhoneList)
+            {
+                x.Showlist();
+            }
+        }
+
+        List<PhoneBook> PhoneList = new List<PhoneBook>();
+
+        public List<PhoneBook> PhoneList1
+        {
+            get { return PhoneList; }
+            set { PhoneList = value; }
+        }
+
+        public PhoneBook()
+        {
+        }
+
+        public PhoneBook(string name, string phone) : base(name, phone)
+        {
+        }
+
+        public PhoneBook(string phone) : base(phone)
+        {
+        }
+
+        
+
+        protected override void InsertPhone(string name, string phone)
+        {
+            foreach (PhoneBook x in PhoneList)
+            {
+                if (!x.Name.Equals(name))
+                {
+                    PhoneList.Add(new PhoneBook(name,phone));
+                }
+                else
+                {
+                    foreach (PhoneBook y in PhoneList)
+                    {
+                        if (!y.Phone1.Equals(phone))
+                        {
+                            PhoneList.Add(new PhoneBook(phone));
+                        }
+                    }
+                }
+            } 
+        }
+
+        protected override void RemovePhone(string name)
+        {
+            for (int i = 0; i < PhoneList.Count; i++)
+            {
+                foreach (PhoneBook x in PhoneList)
+                {
+                    if (x.Name.Equals(name))
+                    {
+                        PhoneList.RemoveAt(i);
+                    }
+                }
+            }
+        }
+
+        protected override void UpdatePhone(string name, string newPhone)
+        {
+            foreach (PhoneBook x in PhoneList)
+            {
+                if (x.Name.Equals(name))
+                {
+                    x.Phone1 = newPhone;
+                }
+            }   
+        }
+
+        protected override void SearchPhone(string name)
+        {
+            foreach (PhoneBook x in PhoneList)
+            {
+                if (x.Name.Equals(name))
+                {    
+                    Console.WriteLine("name : " +x.Name );
+                    Console.WriteLine("phone : " +x.Phone1);
+                }
+            }
+        }
+    }
+}
