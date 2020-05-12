36ce6d741764c698b79f0161a620614f6f4fd943
28111999lenhathoang <53230244+28111999lenhathoang@users.noreply.github.com>
2020-05-12 10:51:30 +0700

    ex2

---
+++ ex2/Programs.cs
@@ -0,0 +1,15 @@
+﻿namespace GeometryExample.Exercise2
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string name;
+            int weight;
+            Lion x = new Lion("Lion", 500);
+            Tiger y =new Tiger("Tiger", 600);
+            x.Show();
+            y.Show();
+        }
+    }
+}
